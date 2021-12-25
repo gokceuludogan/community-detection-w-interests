ca871fdee1d4fb952d0eb48be8296ef474c90c16
Gökçe Uludoğan <gokceuludogan@gmail.com>
2021-12-25 22:25:17 +0300

    Update construct_graph.py

--- src/construct_graph.py
+++ src/construct_graph.py
@@ -1,11 +1,12 @@
 import pandas as pd 
 from ontology import Ontology
+
 interactions = pd.read_csv('../data/interactions.csv')
 entities = pd.read_csv('../data/entities.csv')
 
-mentions = interactions[['UserName', 'MentionedUser', 'weights_mention']].to_records(index=False).tolist()
-retweets =  interactions[['UserName', 'retweet', 'weights_rt']].to_records(index=False).tolist()
-ents = entities[["user","interest type", "interest"]].to_records(index=False).tolist()
+mentions = interactions[['UserName', 'MentionedUser', 'weights_mention']].to_records(index=False).tolist()# [:SIZE]
+retweets =  interactions[['UserName', 'retweet', 'weights_rt']].to_records(index=False).tolist()# [:SIZE]
+ents = entities[["user", "interest type", "interest"]].to_records(index=False).tolist()# [:SIZE]
 
 filtered_mentions = [(user1, user2, weight) for user1, user2, weight in mentions if weight != 0]
 filtered_retweets = [(user1, user2, weight) for user1, user2, weight in retweets if weight != 0]
@@ -13,8 +14,8 @@ ents_triplets = [(user, types, interests) for user, types, interests in ents]
 
 
 ontology = Ontology('../interests-v3.owl')
-#ontology.add_interaction_list(filtered_mentions, 'mention')
-#ontology.add_interaction_list(filtered_retweets, 'retweet')
+ontology.add_interaction_list(filtered_mentions, 'mention')
+ontology.add_interaction_list(filtered_retweets, 'retweet')
 ontology.add_entity_list(ents_triplets)
-ontology.save('../data/populated_interests.owl')
+ontology.save('../data/populated_interests_1000.owl')
 
