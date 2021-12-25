b8d163dbb2ab3a5430e10432924fa9270ed6e9d8
Gökçe Uludoğan <gokceuludogan@gmail.com>
2021-12-25 22:24:41 +0300

    Update community_detection.py

--- src/community_detection.py
+++ src/community_detection.py
@@ -1,18 +1,56 @@
 import networkx as nx
 import json
-
+import time
+import logging
 from pathlib import Path
 from cdlib import algorithms, evaluation
 from ontology import Ontology
 
-ontology = Ontology('../data/populated_interests.owl')
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s', level=logging.INFO)
+
+logging.info('Loading ontology...')
+ontology = Ontology('../data/populated_interests.owl') # sample.owl')
+
+logging.info('Retrieving mentions...')
 mentions = ontology.get_all_interactions('mention')
+
+logging.info('Retrieving retweets...')
 retweets = ontology.get_all_interactions('retweets')
 
 mention_dict = {(user1, user2): weight for user1, user2, weight in mentions}
 retweet_dict = {(user1, user2): weight for user1, user2, weight in retweets}
 
-edges = {key: mention_dict.get(key, 0) + retweet_dict.get(key, 0) for key in set(mention_dict.keys()).union(set(retweet_dict.keys()))}
+logging.info('Retrieving interests...')
+
+interests = ontology.get_class_based_interests()
+#interests = ontology.get_all_interests()
+
+logging.info('Finding user interest similarities')
+interest_dict = {}
+for user1, interest1, user2, interest2, interest_type in interests:
+	if (user1, user2) not in interest_dict:
+		interest_dict[(user1, user2)] = {interest_type: {'common': interest1 == interest2, 'count': 1}}
+	else:
+		if interest_type not in interest_dict[(user1, user2)]:
+			interest_dict[(user1, user2)][interest_type] = {'common': interest1 == interest2, 'count': 1}
+		else:
+			interest_dict[(user1, user2)][interest_type]['common'] += (interest1 == interest2)
+			interest_dict[(user1, user2)][interest_type]['count'] += 1
+
+interest_user_sim = {}
+for user1, user2 in interest_dict:
+	counts = interest_dict[(user1, user2)]
+	interest_user_sim[(user1, user2)] = [counts[interest_type]['common'] / counts[interest_type]['count'] for interest_type in counts]	
+	
+
+
+logging.info('Constructing homogenoues graph')
+user_interest_dict = {pair: sum(class_sim)/len(class_sim) for pair, class_sim in interest_user_sim.items()}
+
+all_pairs = set(mention_dict).union(set(retweet_dict)).union(set(user_interest_dict))
+edges = {key: mention_dict.get(key, 0) + retweet_dict.get(key, 0) + user_interest_dict.get(key, 0) for key in all_pairs}
 
 nodes = set([node for pair in edges.keys() for node in pair])
 
@@ -28,9 +66,11 @@ for ix, node in enumerate(nodes):
 for pair, weight in edges.items():
 	G.add_edge(node_mapping[pair[0]], node_mapping[pair[1]], weight=weight)
 
+logging.info('Running Leiden algorithm...')
 coms = algorithms.leiden(G)
 communities = coms.communities
 
+logging.info('Evaluation...')
 avg_internal_degree = coms.average_internal_degree(summary=False)
 link_modularity = coms.link_modularity() # G, communities)
 tpr = coms.triangle_participation_ratio() # G, communities)
@@ -56,7 +96,3 @@ with open(path / 'leiden_scores.json', 'w') as f:
 		#'surprise': surprise, 
 		'tpr': tpr#, 'purity': purity
 		}))
-
-
-
-	
